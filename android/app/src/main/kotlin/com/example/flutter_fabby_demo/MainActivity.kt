ff0a5d918804034e9b067f550dc76f4529e7c05d
abid-appic <abid@appic.me>
2024-09-25 17:46:15 +0530

    member and guest checkout completed

--- android/app/src/main/kotlin/com/example/flutter_fabby_demo/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/flutter_fabby_demo/MainActivity.kt
@@ -1,5 +1,27 @@
 package com.example.flutter_fabby_demo
 
+import android.content.Intent
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugin.common.MethodChannel
+class MainActivity: FlutterActivity(){
+    private val CHANNEL = "deeplink.flutter.dev/channel"
 
-class MainActivity: FlutterActivity()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        MethodChannel(flutterEngine?.dartExecutor?.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "getInitialLink") {
+                result.success(intent?.dataString)
+            }
+        }
+    }
+
+    override fun onNewIntent(intent: Intent) {
+        super.onNewIntent(intent)
+        // Handle new deep link when the app is already running
+        if (intent.action == Intent.ACTION_VIEW) {
+            MethodChannel(flutterEngine?.dartExecutor?.binaryMessenger, CHANNEL).invokeMethod("onDeepLink", intent.dataString)
+        }
+    }
+}
