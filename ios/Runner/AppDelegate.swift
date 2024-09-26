42559895181bb638c57b9aa7e4b5716c008683fe
PranayAppic <94957615+PranayAppic@users.noreply.github.com>
2024-09-26 12:46:45 +0530

    Updated appDelegate

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,13 +1,28 @@
 import Flutter
 import UIKit
 
+//@UIApplicationMain
+//@objc class AppDelegate: FlutterAppDelegate {
+//  override func application(
+//    _ application: UIApplication,
+//    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+//  ) -> Bool {
+//    GeneratedPluginRegistrant.register(with: self)
+//    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+//  }
+//}
+
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    open url: URL,
+    options: [UIApplication.OpenURLOptionsKey : Any] = [:]
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    let link = url.absoluteString
+    let controller = window?.rootViewController as! FlutterViewController
+    let channel = FlutterMethodChannel(name: "com.example.app/deeplink", binaryMessenger: controller.binaryMessenger)
+    channel.invokeMethod("deeplink", arguments: link)
+    return super.application(application, open: url, options: options)
   }
 }
